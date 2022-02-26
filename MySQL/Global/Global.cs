7d433e2bd733b46c4cce571975d1b01489e30518
asher-lab <asher01000001@gmail.com>
2022-02-26 13:39:33 +0800

    commit appdev

---
+++ MySQL/Global/Global.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MySQL
+{
+    public enum Status { Failed, Successful};    
+
+    public static class Database
+    {
+        public static string ConnectionString = "server=appdev-azure-db-myaql-us-prod.mysql.database.azure.com; Password=Password@6666;  Database=test;Uid=appdevuser;";
+
+        public enum ReadingType { DataAdapter, DataReader };
+
+        public enum SearchingType { WithDatabase, NoDatabase };
+    }
+
+    public static class ParsingExtensions
+    {
+        public static int ParseInt (this object value, int defaultValue = -1)
+        {
+            if (value == null)
+                return defaultValue;
+
+            try
+            {
+                return Convert.ToInt32(value);
+            }
+            catch
+            {
+                return defaultValue;
+
+            }
+        }
+
+        public static string ParseString (this object value, string defaultValue = "Invalid")
+        {
+            if (value == null)
+                return defaultValue;
+
+            try
+            {
+                return value.ToString();
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
---
+++ MySQL/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace MySQL
+{  
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MySQLViewModel (this);
+        }
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }    
+}
